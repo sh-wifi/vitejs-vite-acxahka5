f7ed02a0e647695bb75b748c88dbc84f0db87d9e
sh-wifi <6321058@ed.tus.ac.jp>
2025-08-14 14:31:14 +0900

    1回目の連携

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://lwazdgaomnxwbipmrklh.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3YXpkZ2FvbW54d2JpcG1ya2xoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwNzY5MzksImV4cCI6MjA3MDY1MjkzOX0.19yxEhL4v7_cpiCEjRjjL0-wU7eafL1dBw1kYe4K80Q';
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey);
